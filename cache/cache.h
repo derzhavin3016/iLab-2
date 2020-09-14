2a75639dcde2cd2638105aa3776a75fdcac0cc60
derzhavin3016 <derzhavin.andrej@yandex.ru>
2020-09-14 11:38:35 +0300

    hard version

--- cache/cache.h
+++ cache/cache.h
@@ -25,6 +25,7 @@ private:
   using List_it = typename std::list<type>::iterator;
   using Hash_tbl = std::unordered_map<key_type, std::list<List_it>>;
   using Map_it = typename Hash_tbl::iterator;
+  using Map_lst_it = typename std::list<List_it>::iterator;
   Hash_tbl _hash_table;
 
 public:
@@ -40,20 +41,42 @@ public:
     // get a hash
     key_t val_hash = _Hash(val) % _capacity;
 
+    // find in map by hash value
     auto val_lst = _hash_table.find(val_hash);
     if (val_lst != _hash_table.end())
     {
+      // list with this hash exist
       auto val_cache = _CacheFind(val, val_lst);
       if (val_cache != val_lst->end())
       {
+        // this value exist in cache
         val_cache->counter++;
         return val_cache->value;
       }
+      // value doesn't exist in list, but it's hash exist
 
-      val_lst->push_back(_CacheAdd(val, val_hash));
+      if (_cache.size() >= _capacity)
+      {
+        auto Min_lst = _FindMin();
+        Min_lst->counter = 1;
+        Min_lst->value = val;
+        return val;
+      }
+
+      _cache.push_back(lst_elem{val, 1});
+      val_lst->push_back(_cache.back());
+
       return val;
     }
-
+    // value is fully new to cache
+    if (_cache.size() >= _capacity)
+    {
+      auto Min_lst = _FindMin();
+      Min_lst->counter = 1;
+      Min_lst->value = val;
+      Min_lst
+      return val;
+    }
     _hash_table.insert(val_hash, std::list<List_it>{_CacheAdd(val, val_hash)});
   }
 
@@ -71,23 +94,25 @@ private:
     return val;
   }
 
-  void _CacheAdd( const type &val, key_t hash )
+  Map_lst_it _CacheAdd( const type &val, bool IsNew )
   {
-
+    if (_cache.size() >= _capacity)
+      auto Min_lst = _FindMin();
 
   }
 
-  List_it _FindMin( void )
+  Map_lst_it _FindMin( void )
   {
-    size_t min = _hash_table.begin()->beign()->counter;
-    List_it it_min = _hash_table.begin()->beign();
+    auto Min_lst = _hash_table.begin()->begin();
 
     for (auto &mp : _hash_table)
       for (auto &lst : mp)
       {
-        if (lst->counter < min)
-          min =
+        if (lst->counter < Min_lst->counter)
+          Min_lst = lst;
       }
+
+    return Min_lst;
   }
   List_it _CacheFind( const type &value, const Map_it & mit )
   {
