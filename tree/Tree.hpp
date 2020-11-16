12541dac58310a7ca435acba4d6d94b5c2cbaa70
derzhavin3016 <derzhavin.andrej@yandex.ru>
2020-11-16 17:13:47 +0300

    initial

---
+++ tree/Tree.hpp
@@ -0,0 +1,23 @@
+#ifndef TREE_TREE_HPP
+#define TREE_TREE_HPP
+
+namespace ad6
+{
+  template <typename T>
+  class Tree final
+  {
+  private:
+    struct Node
+    {
+
+    };
+
+  public:
+
+    Tree( void );
+  };
+}
+
+#include "Tree.ii"
+
+#endif //TREE_TREE_HPP
