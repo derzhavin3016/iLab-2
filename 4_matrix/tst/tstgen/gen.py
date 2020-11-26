3a45f2c56c810eebc841e821e1d500db34a222d0
derzhavin3016 <derzhavin.andrej@yandex.ru>
2020-11-26 11:24:57 +0300

    new tests added

---
+++ 4_matrix/tst/tstgen/gen.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import numpy as np
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description='Genrator of matrix for test')
+    parser.add_argument('size', metavar='SIZE', type=int, help='size of a matrix')
+    parser.add_argument('fr', metavar='FROM', type=int, help='start value for generate numbers')
+    parser.add_argument('to', metavar='TO', type=int, help='end value for generate numbers')
+
+    args = parser.parse_args()
+
+    if args.size <= 0:
+        print('Incorrect size: ' + str(args.size) + '\nTry again')
+        return
+
+    matr = np.random.randint(args.fr, args.to, (args.size, args.size))
+    det = np.linalg.det(matr)
+
+    print('Det = ' + str(det))
+    print(args.size)
+    for i in range(args.size):
+        for j in range(args.size):
+            print(matr[i][j], end=' ')
+        print()
+
+
+if __name__ == '__main__':
+    main()
